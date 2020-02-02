d7163532c58dcca04f64f2a54aa8b9c9148984c1
Yifan <jameswu1212@gmail.com>
2020-02-02 20:53:55 +0800

    add encoding categorical data

--- Part 1 - Data Preprocessing/Sec2P1/Data_Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Sec2P1/Data_Preprocessing/data_preprocessing_template.py
@@ -17,14 +17,12 @@ imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data
-
-
-# Splitting the dataset into the Training set and Test set
-# from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-# Feature Scaling 
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)"""
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder 
+from sklearn.compose import ColumnTransformer
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+ct = ColumnTransformer(
+    [('one_hot_encoder', OneHotEncoder(), [0])],
+    remainder='passthrough'
+)
+X = ct.fit_transform(X)
