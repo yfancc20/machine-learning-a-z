d8507502ade2980b16fd4b9f035efba46fb67cd4
Yifan <jameswu1212@gmail.com>
2020-02-02 21:54:52 +0800

    finish part1

--- Part 1 - Data Preprocessing/Sec2P1/Data_Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Sec2P1/Data_Preprocessing/data_preprocessing_template.py
@@ -6,23 +6,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('')
 X = dataset.iloc[:, :-1].values # independent variable
-y = dataset.iloc[:, 3].values   # dependent variable
+Y = dataset.iloc[:, 3].values   # dependent variable
 
-# Taking care of missing data
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+# Splitting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder 
-from sklearn.compose import ColumnTransformer
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-ct = ColumnTransformer(
-    [('one_hot_encoder', OneHotEncoder(), [0])],
-    remainder='passthrough'
-)
-X = ct.fit_transform(X)
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
